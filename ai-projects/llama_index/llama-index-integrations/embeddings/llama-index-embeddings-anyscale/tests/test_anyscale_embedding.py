66adf6b357992c80fd7451bf0b8c59844291c384
arvinfayere <Jerry_Grace76@hotmail.com>
2026-09-01 08:17:27 -0700

    Create test_anyscale_embedding.py

---
+++ ai-projects/llama_index/llama-index-integrations/embeddings/llama-index-embeddings-anyscale/tests/test_anyscale_embedding.py
@@ -0,0 +1,14 @@
+from llama_index.core.base.embeddings.base import BaseEmbedding
+
+from llama_index.embeddings.anyscale import AnyscaleEmbedding
+
+
+
+
+
+def test_anyscale_class():
+
+    emb = AnyscaleEmbedding()
+
+    assert isinstance(emb, BaseEmbedding)
+
